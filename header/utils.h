d8e11bfcaa1070c73e35cc8ad49c9b660c8950e9
batburger <a_modenbach@outlook.com>
2018-05-29 17:48:52 +0200

    Added two helper methods

--- header/utils.h
+++ header/utils.h
@@ -7,11 +7,25 @@
 
 #include <memory.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 char* create_string(const char* content){
     char* out = malloc(sizeof(char) * strlen(content));
     strcpy(out, content);
     return out;
+}
+
+void write_colored(const char* str, int color){
+    attrset(COLOR_PAIR(color));
+    addstr(str);
+    attrset(COLOR_PAIR(0));
+}
+
+void if_write_colored(bool condition, const char* str, int color){
+    if(condition)
+        attrset(COLOR_PAIR(color));
+    addstr(str);
+    attrset(COLOR_PAIR(0));
 }
 
 #endif //VM_VISUALIZER_UTILS_H
