8cfddadbfd769ce0045e185a8c0fd26e76b6c59c
batburger <a_modenbach@outlook.com>
2018-06-02 11:18:23 +0200

    Added function to save the program

--- header/io.h
+++ header/io.h
@@ -7,6 +7,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include "program.h"
 
 int load_file(const char *filename, unsigned char **result, size_t* size) {
     *size = 0;
@@ -30,6 +31,33 @@ int load_file(const char *filename, unsigned char **result, size_t* size) {
     return 0;
 }
 
+int save_file(const char *filename, program_t *program){
+    FILE* f = fopen(filename, "wb");
+    if(f == NULL)
+        return  - 1;
 
+    uint8_t *raw_data = malloc(sizeof(uint8_t) * program->raw_size);
+    line_t *line = NULL;
+    uint32_t data_index = 0;
+
+    for(size_t i = 0; i < program->line_amount; i++){
+        line = program->lines[i];
+
+        raw_data[data_index] = line->instruction_opt;
+        for(size_t j = 0; j < line->instruction_args_amount; j++){
+            raw_data[data_index + j + 1] = line->instruction_args[j];
+        }
+        data_index += line->line_raw_size;
+    }
+
+    if(fwrite(raw_data, sizeof(uint8_t), program->raw_size, f) < program->raw_size){
+        free(raw_data);
+        return  -2;
+    }
+
+    free(raw_data);
+    fclose(f);
+    return 0;
+}
 
 #endif //VM_VISUALIZER_IO_H
